5fcadfcc68cf388bccb0550d00be69d9313ab039
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-01 18:45:42 +0300

    feat: operations made sliding

--- src/components/HomeOperations.svelte
+++ src/components/HomeOperations.svelte
@@ -1,7 +1,28 @@
 <script>
   import OperationCard from "./OperationCard.svelte";
+  import { Swiper, SwiperSlide } from "swiper/svelte";
+
+  import { FreeMode } from "swiper";
+
+  import "swiper/css";
+  import "swiper/css/free-mode";
 
   const operations = [
+    {
+      type: "expense",
+      amount: 49,
+      category: "food",
+    },
+    {
+      type: "income",
+      amount: 500,
+      category: "transfer",
+    },
+    {
+      type: "expense",
+      amount: 216,
+      category: "oil",
+    },
     {
       type: "expense",
       amount: 49,
@@ -23,15 +44,25 @@
 <div class="block">
   <h4 class="block-title">İşlem Dağılımı</h4>
   <div class="block-operations">
-    {#each operations as operation}
-      <OperationCard {...operation} />
-    {/each}
+    <Swiper
+      class="operations-slider"
+      freeMode
+      spaceBetween={20}
+      slidesPerView="auto"
+      modules={[FreeMode]}
+    >
+      {#each operations as operation}
+        <SwiperSlide>
+          <OperationCard {...operation} />
+        </SwiperSlide>
+      {/each}
+    </Swiper>
   </div>
 </div>
 
 <style lang="scss">
   @import "../styles/colors";
-  
+
   .block {
     margin-top: 2rem;
 
@@ -43,8 +74,27 @@
     }
 
     &-operations {
-      display: flex;
       margin-top: 1rem;
+      margin-left: -1rem;
+      margin-right: -1rem;
     }
+  }
+
+  :global(.operations-slider) {
+    height: 150px;
+    width: calc(100%);
+    padding-left: 1rem;
+  }
+
+  :global(.operations-slider .swiper-wrapper) {
+    padding-left: 1rem;
+  }
+
+  :global(.swiper-slide) {
+    width: auto !important;
+  }
+
+  :global(.swiper-slide:last-child) {
+    margin-right: 2rem;
   }
 </style>
