e38eb38da98a61920ca1c408505b691ab26366f7
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-02 01:38:01 +0300

    feat: created recent transactions block

---
+++ src/components/RecentTransactions.svelte
@@ -0,0 +1,10 @@
+<script>
+  import { link } from 'svelte-spa-router'
+</script>
+
+<div class="recents">
+  <header class="recents-header">
+    <h2 class="recents-title">Son İşlemler</h2>
+    <a href="/transactions" use:link>Tümü</a>
+  </header>
+</div>
