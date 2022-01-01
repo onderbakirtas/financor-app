2bbfc82f2d9f0a945d94b8017a4c119800f5bd98
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-01 03:47:32 +0300

    feat: iconified tabbar

--- src/components/Tabbar.svelte
+++ src/components/Tabbar.svelte
@@ -6,25 +6,31 @@
     {
       text: "Ana Sayfa",
       link: "/home",
+      icon: "house-door",
     },
     {
       text: "İşlemler",
       link: "/transactions",
+      icon: "arrow-left-right",
     },
     {
       text: "Birikimler",
       link: "/savings",
+      icon: "cash-stack",
     },
   ];
 </script>
 
 <nav class="tabbar">
   {#each tabs as item}
-    <a href={item.link} use:link use:active>{item.text}</a>
+    <a href={item.link} use:link use:active>
+      <i class="bi bi-{item.icon}" />
+    </a>
   {/each}
 </nav>
 
-<style>
+<style lang="scss">
+  @import "../styles/colors";
   nav {
     background-color: #fff;
     display: flex;
@@ -39,27 +45,41 @@
     box-shadow: 0 -5px 25px rgba(20, 30, 40, 0.15);
     padding: 8px;
     height: var(--tabbar-height);
-  }
+    font-size: 1.5rem;
 
-  nav a {
-    display: inline-flex;
-    align-items: center;
-    justify-content: center;
-    height: calc(var(--tabbar-height) - 1rem);
-    flex: 1 1 25%;
-    width: 100px;
-    border-radius: 50px;
-    color: #234;
-    font-weight: 700;
-    text-decoration: none;
-  }
+    & a {
+      display: inline-flex;
+      align-items: center;
+      justify-content: center;
+      height: calc(var(--tabbar-height) - 1rem);
+      flex: 1 1 25%;
+      width: 72px;
+      border-radius: 50px;
+      color: $c-black;
+      font-weight: 700;
+      text-decoration: none;
+      margin-right: 5px;
 
-  nav a:hover {
-    color: royalblue;
-    background-color: #eef;
+      &:last-child {
+        margin-right: 0;
+      }
+
+      &:hover {
+        color: $c-blue;
+        background-color: transparentize($color: $c-blue, $amount: 0.9);
+      }
+    }
   }
 
   :global(nav.tabbar a.active) {
-    color: royalblue;
+    color: #fff;
+    color: $c-blue;
+    background-color: $c-blue;
+    background-color: transparentize($color: $c-blue, $amount: 0.8);
+  }
+
+  :global(a.active:hover) {
+    background-color: transparentize($color: $c-blue, $amount: 0.8) !important;
+    color: $c-blue !important;
   }
 </style>
