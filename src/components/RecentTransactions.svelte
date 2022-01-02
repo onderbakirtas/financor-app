a8b3ca143390dde229155d629a2e6fc29e12e17e
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-02 14:11:45 +0300

    feat: added transaction to recent transactions

--- src/components/RecentTransactions.svelte
+++ src/components/RecentTransactions.svelte
@@ -1,5 +1,6 @@
 <script>
-  import { link } from 'svelte-spa-router'
+  import { link } from "svelte-spa-router";
+  import Transaction from "./Transaction.svelte";
 </script>
 
 <div class="recents">
@@ -7,4 +8,22 @@
     <h2 class="recents-title">Son İşlemler</h2>
     <a href="/transactions" use:link>Tümü</a>
   </header>
+  <main class="recents-list">
+    <Transaction />
+  </main>
 </div>
+
+<style lang="scss">
+  .recents {
+    margin-top: 2rem;
+
+    &-header {
+      display: flex;
+      justify-content: space-between;
+    }
+
+    &-title {
+      font-size: 1.5rem;
+    }
+  }
+</style>
---
+++ src/components/Transaction.svelte
@@ -0,0 +1,10 @@
+<div class="transaction">
+  <span class="transaction-icon">
+    <i class="bi bi-cup-straw" />
+  </span>
+  <div class="transaction-body">
+    <span>Yemek</span>
+    <span>Bugün</span>
+  </div>
+  <div class="tranaction-amount">-15.00 Tl</div>
+</div>
