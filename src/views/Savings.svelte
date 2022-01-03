5143131c9b3bf82f469911fc5fae4b67e1954612
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-04 01:00:43 +0300

    feat: added saving cards to saving view

--- src/views/Savings.svelte
+++ src/views/Savings.svelte
@@ -1,5 +1,48 @@
 <script>
   import { fade } from "svelte/transition";
+  import Saving from "../components/Savings/Saving.svelte";
+
+  const savings = [
+    {
+      title: "Yeni Telefon",
+      target: 15000,
+      dueTo: "01-09-2022",
+      type: "phone-fill",
+    },
+    {
+      title: "Yeni Telefon",
+      target: 15000,
+      dueTo: "01-09-2022",
+      type: "phone-fill",
+    },
+    {
+      title: "Yeni Telefon",
+      target: 15000,
+      dueTo: "01-09-2022",
+      type: "phone-fill",
+    },
+    {
+      title: "Yeni Telefon",
+      target: 15000,
+      dueTo: "01-09-2022",
+      type: "phone-fill",
+    },
+  ];
 </script>
 
-<div in:fade>Birikimler</div>
+<div class="page" in:fade>
+  {#each savings as saving}
+    <div class="box">
+      <Saving {...saving} />
+    </div>
+  {/each}
+</div>
+
+<style lang="scss">
+  @import "../styles/placeholders";
+
+  .box {
+    @extend %box;
+    margin-bottom: 1rem;
+  }
+</style>
