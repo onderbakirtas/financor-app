77a23c9e4d10b94df709eef10ab599e82015c1cb
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-04 00:20:34 +0300

    feat: created saving block

---
+++ src/components/Savings/Saving.svelte
@@ -0,0 +1,32 @@
+<script>
+  import { store } from "../../store";
+
+  export let title;
+  export let target;
+  export let duration;
+  export let type;
+
+  let percentage = $store.balance / target;
+</script>
+
+<div class="saving-card">
+  <div class="saving-card-top">
+    <span class="icon">
+      <i class="bi bi-{type}" />
+    </span>
+    <div class="content">
+      <span>{title}</span>
+      <span>{duration}</span>
+    </div>
+    <div class="percentage">
+      <span>% {percentage}</span>
+    </div>
+  </div>
+  <div class="saving-card-bottom">
+    <progress max={target} value={$store.balance - target} />
+    <div class="values">
+      <span>{$store.balance - target} TL</span>
+      <span>{target} TL</span>
+    </div>
+  </div>
+</div>
