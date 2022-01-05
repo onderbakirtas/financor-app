e8486c53150713aab7c9a31c4bafe4a1ffd3bec1
Onder Bakirtas <onderbakirtas@gmail.com>
2022-01-06 00:44:39 +0300

    fix: recent transactions header style update

--- src/components/Home/AccountCard.svelte
+++ src/components/Home/AccountCard.svelte
@@ -1,5 +1,5 @@
 <script>
-  import { store } from "../../../store";
+  import { store } from "../../store";
 </script>
 
 <div class="card">
--- src/components/Home/RecentTransactions.svelte
+++ src/components/Home/RecentTransactions.svelte
@@ -4,8 +4,8 @@
 </script>
 
 <div class="recents">
-  <header class="recents-header">
-    <h2 class="recents-title">Son İşlemler</h2>
+  <header class="block-header">
+    <h2 class="block-header-title">Son İşlemler</h2>
     <a href="/transactions" use:link>Tümü</a>
   </header>
   <main class="recents-list">
@@ -21,15 +21,6 @@
 
   .recents {
     margin-top: 2rem;
-
-    &-header {
-      display: flex;
-      justify-content: space-between;
-    }
-
-    &-title {
-      font-size: 1.25rem;
-    }
 
     &-list {
       @extend %box;
